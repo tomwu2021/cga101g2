8b09fafc69dd024b83cae52561578673315a4bd6
MinHao1103 <50913433+MinHao1103@users.noreply.github.com>
2022-04-13 09:21:22 +0800

    member update

--- src/main/java/com/members/model/MembersDAO_interface.java
+++ src/main/java/com/members/model/MembersDAO_interface.java
@@ -1,7 +1,27 @@
 package com.members.model;
-import com.common.model.JDBC_DAO_Interface;
+import com.common.model.JDBCDAO_Interface;
 
-public interface MembersDAO_interface extends JDBC_DAO_Interface<MembersVO>{
+public interface MembersDAO_interface extends JDBCDAO_Interface<MembersVO>{
 	
+	// update 情境二：管理員可以修改此會員的狀態(停權/正常)
+	public boolean changeStatus(MembersVO membersVO);
+
+	// update 情境三：管理員可以發送紅利
+	public boolean changeBonus(MembersVO membersVO);
 	
+	// update 情境四：會員忘記密碼 ( password )
+	
+	// select 情境一：會員查詢會員等級 ( RANK_ID )
+	
+	// select 情境二：會員查詢錢包餘額 ( E_WALLET_AMOUNT )
+	
+	// select 情境三：會員查詢紅利帳戶 ( BONUS_AMOUNT	 )
+	
+	// select 情境一：管理員查詢某一筆會員資料 (透過 account)
+	
+	// select 情境二：管理員查詢所有會員資料
+	
+	// select 情境三：管理員模糊查詢某地區的所有會員 ( address )
+	
+	// select 情境四：管理員查詢所有被停權的所有會員 ( status )
 }
---
+++ src/main/java/com/members/model/TestMembersDAO.java
@@ -0,0 +1,57 @@
+package com.members.model;
+
+public class TestMembersDAO {
+
+	public static void main(String[] args) {
+		MembersJDBCDAO dao = new MembersJDBCDAO();
+
+		// insert 情境一：會員辦帳號時，輸入資料庫的內容
+		/*MembersVO membersVO1 = new MembersVO();
+		membersVO1.setAccount("spring@pet.com"); // 動態
+		membersVO1.setPassword("!QAZ2wsx"); // 動態
+		dao.insert(membersVO1);
+		System.out.println(membersVO1);*/
+		
+
+		// update 情境一：會員可修改的資料　PASSWORD	NAME　ADDRESS　PHONE　E_WALLET_PASSWORD	
+		/*MembersVO membersVO2 = new MembersVO();
+		membersVO2.setMemberId(18);
+		membersVO2.setPassword("!QAZ2wsx");
+		membersVO2.setName("方炯冰");
+		membersVO2.setAddress("花蓮縣豐濱鄉大港口20號");
+		membersVO2.setPhone("0946825555");
+		membersVO2.seteWalletPassword("773516");
+		dao.update(membersVO2);*/
+
+		// update 情境二：管理員可以修改此會員的狀態(停權/正常)
+		/*MembersVO membersVO3 = new MembersVO();
+		membersVO3.setMemberId(20);
+		membersVO3.setStatus(0);
+		dao.changeStatus(membersVO3);*/
+		
+		// update 情境三：管理員可以發送紅利
+		/*MembersVO membersVO4 = new MembersVO();
+		membersVO4.setMemberId(20);
+		membersVO4.setBonusAmount(222);
+		dao.changeBonus(membersVO4);*/
+		
+		// update 情境四：會員忘記密碼 ( password )
+		
+		// select 情境一：會員查詢會員等級 ( RANK_ID	 )
+		
+		// select 情境二：會員查詢錢包餘額 ( E_WALLET_AMOUNT )
+		
+		// select 情境三：會員查詢紅利帳戶 ( BONUS_AMOUNT	 )
+		
+		// select 情境一：管理員查詢某一筆會員資料 (透過 account)
+		
+		// select 情境二：管理員查詢所有會員資料
+		
+		// select 情境三：管理員模糊查詢某地區的所有會員 ( address )
+		
+		// select 情境四：管理員查詢所有被停權的所有會員 ( status )
+		
+		
+	}
+
+}
