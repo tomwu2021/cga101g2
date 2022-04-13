c06ce5b0211c2d9e5aaefad9c5b96c20cbd44195
MinHao1103 <50913433+MinHao1103@users.noreply.github.com>
2022-04-13 15:06:09 +0800

    update member

--- src/main/java/com/members/model/MembersDAO_interface.java
+++ src/main/java/com/members/model/MembersDAO_interface.java
@@ -1,27 +1,30 @@
 package com.members.model;
+
 import com.common.model.JDBCDAO_Interface;
 
-public interface MembersDAO_interface extends JDBCDAO_Interface<MembersVO>{
-	
-	// update 情境二：管理員可以修改此會員的狀態(停權/正常)
+public interface MembersDAO_interface extends JDBCDAO_Interface<MembersVO> {
+
+	// update 情境三：管理員可以修改此會員的狀態(停權/正常)
 	public boolean changeStatus(MembersVO membersVO);
 
-	// update 情境三：管理員可以發送紅利
+	// update 情境四：管理員可以發送紅利
 	public boolean changeBonus(MembersVO membersVO);
+
+	// update 情境五：會員忘記密碼 ( password )
+	public boolean forgotPassword(MembersVO membersVO);
+
+	// select 情境六：會員查詢會員等級 ( RANK_ID )
+	public int selectRankId(MembersVO membersVO);
+
+	// select 情境七：會員查詢錢包餘額 ( E_WALLET_AMOUNT )
+	public int selectEWalletAmount(MembersVO membersVO);
 	
-	// update 情境四：會員忘記密碼 ( password )
-	
-	// select 情境一：會員查詢會員等級 ( RANK_ID )
-	
-	// select 情境二：會員查詢錢包餘額 ( E_WALLET_AMOUNT )
-	
-	// select 情境三：會員查詢紅利帳戶 ( BONUS_AMOUNT	 )
-	
-	// select 情境一：管理員查詢某一筆會員資料 (透過 account)
-	
-	// select 情境二：管理員查詢所有會員資料
-	
-	// select 情境三：管理員模糊查詢某地區的所有會員 ( address )
-	
-	// select 情境四：管理員查詢所有被停權的所有會員 ( status )
+	// select 情境八：會員查詢紅利帳戶 ( BONUS_AMOUNT )
+	public int selectBonusAmount(MembersVO membersVO);
+
+	// select 情境九：管理員查詢某一筆會員資料 (透過 account)
+
+	// select 情境十：管理員查詢所有會員資料
+
+	// select 情境十一：管理員查詢所有被停權的所有會員 ( status )
 }
--- src/main/java/com/members/model/TestMembersDAO.java
+++ src/main/java/com/members/model/TestMembersDAO.java
@@ -7,13 +7,13 @@ public class TestMembersDAO {
 
 		// insert 情境一：會員辦帳號時，輸入資料庫的內容
 		/*MembersVO membersVO1 = new MembersVO();
-		membersVO1.setAccount("spring@pet.com"); // 動態
+		membersVO1.setAccount("Redis@pet.com"); // 動態
 		membersVO1.setPassword("!QAZ2wsx"); // 動態
 		dao.insert(membersVO1);
 		System.out.println(membersVO1);*/
 		
 
-		// update 情境一：會員可修改的資料　PASSWORD	NAME　ADDRESS　PHONE　E_WALLET_PASSWORD	
+		// update 情境二：會員可修改的資料　PASSWORD	NAME　ADDRESS　PHONE　E_WALLET_PASSWORD	
 		/*MembersVO membersVO2 = new MembersVO();
 		membersVO2.setMemberId(18);
 		membersVO2.setPassword("!QAZ2wsx");
@@ -23,33 +23,43 @@ public class TestMembersDAO {
 		membersVO2.seteWalletPassword("773516");
 		dao.update(membersVO2);*/
 
-		// update 情境二：管理員可以修改此會員的狀態(停權/正常)
+		// update 情境三：管理員可以修改此會員的狀態(停權/正常)
 		/*MembersVO membersVO3 = new MembersVO();
 		membersVO3.setMemberId(20);
 		membersVO3.setStatus(0);
 		dao.changeStatus(membersVO3);*/
 		
-		// update 情境三：管理員可以發送紅利
+		// update 情境四：管理員可以發送紅利
 		/*MembersVO membersVO4 = new MembersVO();
 		membersVO4.setMemberId(20);
 		membersVO4.setBonusAmount(222);
 		dao.changeBonus(membersVO4);*/
 		
-		// update 情境四：會員忘記密碼 ( password )
+		// update 情境五：會員忘記密碼 ( password )
+		/*MembersVO membersVO5 = new MembersVO();
+		membersVO5.setMemberId(22);
+		dao.forgotPassword(membersVO5);*/
 		
-		// select 情境一：會員查詢會員等級 ( RANK_ID	 )
+		// select 情境六：會員查詢會員等級 ( RANK_ID )
+		/*MembersVO membersVO6 = new MembersVO();
+		membersVO6.setMemberId(8);
+		System.out.println(dao.selectRankId(membersVO6));*/
 		
-		// select 情境二：會員查詢錢包餘額 ( E_WALLET_AMOUNT )
+		// select 情境七：會員查詢錢包餘額 ( E_WALLET_AMOUNT )
+		/*MembersVO membersVO7 = new MembersVO();
+		membersVO7.setMemberId(17);
+		System.out.println(dao.selectEWalletAmount(membersVO7));*/
 		
-		// select 情境三：會員查詢紅利帳戶 ( BONUS_AMOUNT	 )
+		// select 情境八：會員查詢紅利帳戶 ( BONUS_AMOUNT	 )
+		MembersVO membersVO8 = new MembersVO();
+		membersVO8.setMemberId(20);
+		System.out.println(dao.selectBonusAmount(membersVO8));
 		
-		// select 情境一：管理員查詢某一筆會員資料 (透過 account)
+		// select 情境九：管理員查詢某一筆會員資料 (透過 account)
 		
-		// select 情境二：管理員查詢所有會員資料
+		// select 情境十：管理員查詢所有會員資料
 		
-		// select 情境三：管理員模糊查詢某地區的所有會員 ( address )
-		
-		// select 情境四：管理員查詢所有被停權的所有會員 ( status )
+		// select 情境十一：管理員查詢所有被停權的所有會員 ( status )
 		
 		
 	}
