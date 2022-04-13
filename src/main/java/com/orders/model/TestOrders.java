88b5bcc37c57b97ec7523a0459f6c14582aa962a
cloudy0523 <chrissy8love@gmail.com>
2022-04-13 14:39:43 +0800

    test orders dao

--- src/main/java/com/orders/model/TestOrders.java
+++ src/main/java/com/orders/model/TestOrders.java
@@ -2,6 +2,7 @@ package com.orders.model;
 
 public class TestOrders {
 	public static void main(String[] args) {
+		//新增一筆訂單
 		OrdersJDBCDAO dao=new OrdersJDBCDAO();
 		OrdersVO ordersVO=new OrdersVO();
 		ordersVO.setMemberId(5);
@@ -9,9 +10,9 @@ public class TestOrders {
 		ordersVO.setPhone("0976543218");
 		ordersVO.setAddress("高雄市美麗島捷運站");
 		ordersVO.setSumPrice(750);
+		ordersVO.setBonus(0);
 		ordersVO.setDiscount(0);
 		ordersVO.setPayPrice(750);
 		dao.insert(ordersVO);
 	}
 }
-
