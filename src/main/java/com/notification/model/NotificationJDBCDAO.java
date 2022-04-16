208c466af1d62f8a9f299a819dc9e543341f1b63
MinHao1103 <50913433+MinHao1103@users.noreply.github.com>
2022-04-16 15:41:10 +0800

    upload notification DAO

---
+++ src/main/java/com/notification/model/NotificationJDBCDAO.java
@@ -0,0 +1,139 @@
+package com.notification.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import connection.JDBCConnection;
+
+public class NotificationJDBCDAO implements NotificationDAO_interface {
+
+	Connection con;
+
+// 情境一 insert：新增一筆通知
+	@Override
+	public NotificationVO insert(NotificationVO notificationVO) {
+		con = JDBCConnection.getRDSConnection();
+		NotificationVO notificationVO2 = insert(notificationVO, con);
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return notificationVO2;
+	}
+
+	public NotificationVO insert(NotificationVO notificationVO, Connection con) {
+		final String INSERT_STMT = "INSERT INTO notification(member_id, context, status) values(?,?,0);";
+		if (con != null) {
+			try {
+				PreparedStatement pstmt = con.prepareStatement(INSERT_STMT, Statement.RETURN_GENERATED_KEYS);
+				pstmt.setInt(1, notificationVO.getMemberId());
+				pstmt.setString(2, notificationVO.getContext());
+				pstmt.executeUpdate();
+
+				ResultSet rs = pstmt.getGeneratedKeys();
+				if (rs.next()) {
+					notificationVO.setNotificationId(rs.getInt(1));
+				}
+				return notificationVO;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+// 情境二 select：查詢某會員 id 所有通知
+	@Override
+	public List<NotificationVO> getAllById(Integer id) {
+		con = JDBCConnection.getRDSConnection();
+		List<NotificationVO> list = getAllById(id, con);
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public List<NotificationVO> getAllById(Integer id, Connection con) {
+
+		final String SELECT_ONE_BYID = "select notification_id, member_id, context, time, status from notification where member_id = ?;";
+
+		if (con != null) {
+			try {
+				PreparedStatement pstmt = con.prepareStatement(SELECT_ONE_BYID);
+				pstmt.setInt(1, id);
+				ResultSet rs = pstmt.executeQuery();
+				List<NotificationVO> list = new ArrayList<>();
+				while (rs.next()) {
+					NotificationVO newNotification = new NotificationVO();
+					newNotification.setNotificationId(rs.getInt("notification_id"));
+					newNotification.setMemberId(rs.getInt("member_id"));
+					newNotification.setContext(rs.getString("context"));
+					newNotification.setTime(rs.getTimestamp("time"));
+					newNotification.setStatus(rs.getInt("status"));
+					list.add(newNotification);
+				}
+				return list;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return null;
+	}
+
+// 情境三 update：會員查看通知後，status 變成已讀 1
+	@Override
+	public Boolean updateStatus(NotificationVO notificationVO) {
+		con = JDBCConnection.getRDSConnection();
+		Boolean b = updateStatus(notificationVO, con);
+		try {
+			con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return b;
+	}
+
+	public Boolean updateStatus(NotificationVO notificationVO, Connection con) {
+		final String UPDATE = "UPDATE notification SET status = 1 where notification_id = ?;";
+
+		if (con != null) {
+			try {
+				PreparedStatement pstmt = con.prepareStatement(UPDATE);
+				pstmt.setInt(1, notificationVO.getNotificationId());
+				pstmt.executeUpdate();
+				return true;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public List<NotificationVO> getAll() {
+		return null;
+	}
+
+	@Override
+	public NotificationVO update(NotificationVO notificationVO) {
+		return null;
+	}
+
+	@Override
+	public boolean delete(NotificationVO notificationVO) {
+		return false;
+	}
+
+	@Override
+	public NotificationVO getOneById(Integer id) {
+		return null;
+	}
+
+}
---
+++ src/main/java/com/notification/model/TestNotificationDAO.java
@@ -0,0 +1,27 @@
+package com.notification.model;
+
+public class TestNotificationDAO {
+
+	public static void main(String[] args) {
+
+		NotificationJDBCDAO dao = new NotificationJDBCDAO();
+
+		// 情境一 insert：新增一筆通知
+//		NotificationVO notificationVO1 = new NotificationVO();
+//		notificationVO.setMemberId(3); 
+//		notificationVO.setContext("二還閒入但雲意猶為鄰垂楊前夫：上有何當五月⋯東兒女堂前，國此，三遊山川明朝輪臺。青臨洮芙蓉⋯殷勤城臺城何事之然不得，秋青天蹉跎，一身萋萋太故人無情，道之難窮啼風波，難難於關顏色二一。");
+//		System.out.println(dao.insert(notificationVO));
+
+		// 情境二 select：查詢某會員所有通知
+//		for (NotificationVO n : dao.getAllById(3)) {
+//			System.out.println(n);
+//		}
+
+		// 情境三 update：會員查看通知後，status 變成已讀 1
+//		NotificationVO notificationVO2 = new NotificationVO();
+//		notificationVO2.setNotificationId(3);
+//		System.out.println(dao.updateStatus(notificationVO2));
+
+	}
+
+}
