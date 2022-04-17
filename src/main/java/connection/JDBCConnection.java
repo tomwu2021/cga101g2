89020b5676320e1ab9aa9b2aa987215b3e4611c8
fsloveuj <fsloveuj@gmail.com>
2022-04-18 03:14:13 +0800

    test

--- src/main/java/connection/JDBCConnection.java
+++ src/main/java/connection/JDBCConnection.java
@@ -5,19 +5,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class JDBCConnection {
-
-	// MySQL 8之後連線URL需加上SSL與時區設定
-	public final static String URL = "jdbc:mysql://localhost:3306/bookshop_jdbc?useUnicode=yes&characterEncoding=utf8&useSSL=true&serverTimezone=Asia/Taipei";
-	// MySQL 8之前
-	// public final static String URL = "jdbc:mysql://localhost:3306/bookshop_jdbc";
 	
-	public final static String USERNAME = "root";
-	public final static String PASSWORD = "840614";
-	
-//	private static final String USERNAME = "cga_02";
-//	private static final String PASSWORD = "cga_02";
-//	private static final String URL = 
-//			"jdbc:mysql://database-1.czlvj0ycwyxq.ap-northeast-1.rds.amazonaws.com:3306/cga_02?serverTimezone=Asia/Taipei";
+	private static final String USERNAME = "cga_02";
+	private static final String PASSWORD = "cga_02";
+	private static final String URL = 
+			"jdbc:mysql://database-1.czlvj0ycwyxq.ap-northeast-1.rds.amazonaws.com:3306/cga_02?serverTimezone=Asia/Taipei";
 
 	public static Connection getRDSConnection() {
 		Connection con = null;
