b3a0978fc81494b6eadada74ae106d193992066a
Phyllis-Lee <winnie1002375@gmail.com>
2022-04-17 09:53:09 +0800

    update article DAO

--- src/main/java/com/article/model/ArticleDAO_interface.java
+++ src/main/java/com/article/model/ArticleDAO_interface.java
@@ -5,11 +5,23 @@ import com.common.model.JDBCDAO_Interface;
 
 
 public interface ArticleDAO_interface extends JDBCDAO_Interface<ArticleVO>{
-	
-	public ArticleVO insert(ArticleVO articleVO);
-    public boolean delete(ArticleVO articleVO);
-    public ArticleVO update(ArticleVO articleVO);
-    public ArticleVO getOneById(Integer articleID);
-    public List<ArticleVO> getAll();
 
+/** 發布最新消息；
+ * (必)type[0-公告,1-商城], title, content, empNo */
+	public ArticleVO insert(ArticleVO articleVO);
+/** 下架最新消息：
+ * (必)articleId */
+    public boolean delete(ArticleVO articleVO);
+/** 修改最新消息：
+ * (必)articleId, type[0-公告,1-商城], title, content, empNo */
+    public ArticleVO update(ArticleVO articleVO);
+/** 查詢一則最新消息：
+ * (必)articleId */
+    public ArticleVO getOneById(Integer articleID);
+/** 查詢所有最新消息 */
+    public List<ArticleVO> getAll();
+/** 查詢一類最新消息：
+ * (必)type[0-公告,1-商城] */
+    public List<ArticleVO> getAllByType(Integer type);
+    
 }
--- src/main/java/com/article/model/TestArticle.java
+++ src/main/java/com/article/model/TestArticle.java
@@ -10,33 +10,40 @@ public class TestArticle {
 		
 		// 新增
 		ArticleVO aVO1 = new ArticleVO();
-		aVO1.setType(1);
-		aVO1.setTitle("DOREMI");
-		aVO1.setContent("周日公園趣");
+		aVO1.setType(0);
+		aVO1.setTitle("2022/5/2 伺服器暫停服務");
+		aVO1.setContent("嘿嘿嘿不給你買");
 		aVO1.setEmpNo(1);
 		System.out.println(dao.insert(aVO1));
 		
 		// 刪除
-		ArticleVO aVO2 = new ArticleVO();
-		aVO2.setArticleId(1);
-		System.out.println(dao.delete(aVO2));
-		
+//		ArticleVO aVO2 = new ArticleVO();
+//		aVO2.setArticleId(2);
+//		System.out.println(dao.delete(aVO2));
+	
 		// 修改
-		ArticleVO aVO3 = new ArticleVO();
-		aVO3.setType(1);
-		aVO3.setTitle("news");
-		aVO3.setContent("content here");
-		aVO3.setEmpNo(1);
-		aVO3.setArticleId(2);
-		System.out.println(dao.update(aVO3));
+//		ArticleVO aVO3 = new ArticleVO();
+//		aVO3.setType(1);
+//		aVO3.setTitle("newaaa");
+//		aVO3.setContent(null);
+//		aVO3.setEmpNo(1);
+//		aVO3.setArticleId(3);
+//		System.out.println(dao.update(aVO3));
 	
 		// 查一筆
-		System.out.println(dao.getOneById(3));
-	
+//		System.out.println(dao.getOneById(4));
+
 		// 查全部
-		List<ArticleVO> alist = dao.getAll();
+//		List<ArticleVO> aList = dao.getAll();
+//		
+//		for(ArticleVO aVO:aList) {
+//			System.out.println(aVO);
+//		}
 		
-		for(ArticleVO aVO:alist) {
+		// 查全部
+		List<ArticleVO> tList = dao.getAllByType(0);
+		
+		for(ArticleVO aVO:tList) {
 			System.out.println(aVO);
 		}
 	}
