86b63cf4e43b68f7ff3b64bed9c847d8cc8c750a
fsloveuj <fsloveuj@gmail.com>
2022-04-21 05:37:00 +0800

    動態導覽列完成nav.js update sort1

--- src/main/java/com/sort1/model/TESTSort1MybatisDAO.java
+++ src/main/java/com/sort1/model/TESTSort1MybatisDAO.java
@@ -12,21 +12,27 @@ public class TESTSort1MybatisDAO {
 //		dao.insert(sort1VO);
 
 //更新
-		Sort1VO sort1VO = new Sort1VO();
-		sort1VO.setSort1Id(77);
-		sort1VO.setSort1Name("11331i");
-		dao.updateById(sort1VO);
+//		Sort1VO sort1VO = new Sort1VO();
+//		sort1VO.setSort1Id(77);
+//		sort1VO.setSort1Name("11331i");
+//		dao.updateById(sort1VO);
 		
 //查詢所有
-//		List<Sort1VO> list = dao.selectAll();
-//		System.out.println(list);
+		List<Sort1VO> list = dao.selectAll();
+		System.out.println(list);
 		
 //查詢此Sort1Name是否已有資料,UQ
 //		Sort1VO sort1VO1 = new Sort1VO();
-//		sort1VO1.setSort1Name("KIKI");
+//		sort1VO1.setSort1Name("KI666KI");
 //		sort1VO1 = dao.selectBySort1Name(sort1VO1.getSort1Name());
 //		if(sort1VO1 !=null) {
 //			System.out.println("欄位重複");
 //		}
 	}
+	
+//	public void test()  {
+//		Sort1VO sort1VO1 = new Sort1VO();
+//		sort1VO1.setSort1Name("KI666KI");
+//	}
+	
 }
---
+++ src/main/java/com/sort1/service/Sort1Service.java
@@ -0,0 +1,37 @@
+package com.sort1.service;
+
+import java.util.List;
+
+import com.sort1.model.Sort1MybatisDAO;
+import com.sort1.model.Sort1VO;
+
+public class Sort1Service implements Sort1Service_interface{
+//	為什麼不直接寫 Sort1MybatisDAO dao  = new Sort1MybatisDAO();?
+//	防止別隻CLASS檔經由NEW間接呼叫到DAO,必須要經過商業邏輯層Service的判斷才可以執行DAO?
+	
+//	private:僅類別內有效
+	private Sort1MybatisDAO dao;
+
+//	改寫預設建構子
+	public Sort1Service() {
+		dao = new Sort1MybatisDAO(); 
+	}
+
+	@Override
+	public boolean save(Sort1VO sort1vo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Sort1VO> findAll() {
+		return dao.selectAll();
+	}
+
+	@Override
+	public boolean updateB(Sort1VO sort1vo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
