db3e13f49b87178e8100ce808f047513ee9de4f3
MinHao1103 <50913433+MinHao1103@users.noreply.github.com>
2022-05-13 01:54:48 +0800

    update empSelect.js & delete listAllMember.jsp 、 listOneMember.jsp 、 member_select.jsp

--- src/main/webapp/assets/js/empSelect.js
+++ src/main/webapp/assets/js/empSelect.js
@@ -19,25 +19,25 @@ $(function() {
 				html += "<th>會員帳號</th>";
 				html += "<th>會員姓名</th>";
 				html += "<th>會員地址</th>";
-				html += "<th>會員手機</th>";
-				html += "<th style='width:70px'>會員等級</th>";
-				html += "<th style='width:70px'>會員紅利</th>";
-				html += "<th style='width:70px'>會員狀態</th>";
-				html += "<th>會員建立日期</th>";
+				html += "<th style='width:90px'>會員手機</th>";
+				html += "<th style='width:80px'>會員等級</th>";
+				html += "<th style='width:80px'>會員紅利</th>";
+				html += "<th style='width:80px'>會員狀態</th>";
+				html += "<th style='width:100px'>會員建立日期</th>";
 				html += "<th>修改</th>";
 				html += "</tr>";
 
 				for (let i = 0; i < objectJSON.length; i++) {
 					let memberVO = objectJSON[i];
 					html += "<tr>";
-					html += "<td>"+ memberVO.account +"</td>";
+					html += "<td>"+memberVO.account+"</td>";
 					html += "<td>"+memberVO.name+"</td>";
 					html += "<td>"+memberVO.address+"</td>";
 					html += "<td>"+memberVO.phone+"</td>";
 					html += "<td>"+memberVO.rankId+"</td>";
 					html += "<td>"+memberVO.bonusAmount+"</td>";
 					html += "<td>"+memberVO.status+"</td>";
-					html += "<td>"+memberVO.createTime+"</td>";
+					html += "<td>"+memberVO.createTimeString+"</td>";
 					html += "<td><button onclick='updateInfo'>修改</button></td>";
 					html += "</tr>";
 				}
