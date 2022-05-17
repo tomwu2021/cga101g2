8e04dd3df612218c6211450012bbb930ffcfbea3
cloudy0523 <chrissy8love@gmail.com>
2022-05-17 20:00:56 +0800

    update SelectAllOrder

--- src/main/java/com/allOrders/controller/SelectAllOrder.java
+++ src/main/java/com/allOrders/controller/SelectAllOrder.java
@@ -2,8 +2,10 @@ package com.allOrders.controller;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +18,11 @@ import com.common.model.PageResult;
 import com.google.gson.Gson;
 import com.group_buyer.model.GroupBuyerService;
 import com.group_buyer.model.GroupBuyerVO;
+import com.group_order.model.GroupOrderService;
+import com.group_order.model.GroupOrderVO;
 import com.members.model.MembersVO;
+import com.orders.model.OrdersService;
+import com.orders.model.OrdersVO;
 
 @WebServlet("/AllOrders")
 public class SelectAllOrder extends HttpServlet{
@@ -27,30 +33,30 @@ public class SelectAllOrder extends HttpServlet{
 			throws ServletException, IOException {
 		res.setContentType("application/json; charset=UTF-8");
 		req.setCharacterEncoding("UTF-8");
-		PrintWriter out = res.getWriter();
-		HttpSession session = req.getSession();
-		MembersVO membersVO=(MembersVO) session.getAttribute("membersVO");
-		Integer memberId = membersVO.getMemberId();		
-		//分頁條件
-		Integer thisPage = Integer.parseInt(req.getParameter("thisPage")); // 設置當前頁數
-		Integer pageSize = Integer.parseInt(req.getParameter("pageSize")); // 設置每頁顯示筆數
-		String sort = req.getParameter("sort"); // 設置排序方式 (升降冪)
-		String order = req.getParameter("order"); // 設置排序欄位
-		//客製條件
-		Map<String, Object> map = new HashMap<>(); // 創建多筆指定欄位條件 Map
-		map.put("member_id", memberId);
-		if(req.getParameter("status") != null) {
-			Integer status = Integer.parseInt(req.getParameter("status"));						
-			map.put("o.status", status);			
-		}
+		String action = req.getParameter("action");
+		if ("orderList".equals(action)) {
+			PrintWriter out = res.getWriter();
+			OrdersService ordersService=new OrdersService();
+			List<OrdersVO> list =ordersService.getAll();
+			Gson gson = new Gson();
+			out.write(gson.toJson(list));
+			}
+		if ("groupOrderList".equals(action)) {
+			PrintWriter out = res.getWriter();
+			GroupOrderService groupOrderService=new GroupOrderService();
+			List<GroupOrderVO> list=groupOrderService.getAll();	
+			Gson gson = new Gson();
+			out.write(gson.toJson(list));
+			}
+		if ("updateOrder".equals(action)) {
+			Integer orderId = Integer.parseInt(req.getParameter("orderId"));
+			Integer status = Integer.parseInt(req.getParameter("status"));
+			OrdersService ordersService=new OrdersService();
+			ordersService.updateStatusByOrderId(orderId,status);
+			
+			}
 
-		PageQuery pq = new PageQuery(thisPage, pageSize, sort, order, map); // 創建分頁查訊物件
 		
-		GroupBuyerService groupBuyerService=new GroupBuyerService();
-		PageResult<GroupBuyerVO> rpq = groupBuyerService.getPageResult(pq);
-		Gson gson = new Gson();
-		out.write(gson.toJson(rpq));
-
 
 	}
 	
